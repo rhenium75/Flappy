a0b80a580a8123f302f33e81c81d1bd408743358
Rhenium <Rhenium>
2016-01-19 18:56:36 +0900

    プレイヤーをクラス化。敵の処理を追加。

--- Scenes/Game.hpp
+++ Scenes/Game.hpp
@@ -8,45 +8,64 @@ class Block{
 	public:
 	Rect UpRect, DownRect;
 	Block(double UpY,double DownY){
-		UpRect = Rect(400,-UpY,80,640);
-		DownRect = Rect(400,DownY,80,640);
+		UpRect = Rect(480,-640+UpY,80,640);
+		DownRect = Rect(480,DownY,80,640);
 	}
 	void update(){
+		UpRect.moveBy(-3,0);
+		DownRect.moveBy(-3 ,0);
 
 	}
 	void draw(){
-		UpRect.draw();
-		DownRect.draw();
+		UpRect.draw(Palette::Green);
+		DownRect.draw(Palette::Green);
+	}
+};
+
+class Player{
+public:
+	Circle body;
+	Vec2 Move;
+	Player(){
+		body.setPos(30,560);
+		body.setSize(100);
+		Move = Vec2(0,0);
+	}
+	virtual void update(){
+		body.moveBy(Move);
+		Move.moveBy(0,-0.5);
+		if (Input::MouseL.clicked){
+			Move.set(Move.x, 10);
+		}
+	}
+	virtual void draw() const {
+		TextureAsset(L"Hakka1").draw(body.x, 640 - body.y);
+	}
+	bool IsDeath(){
+		return body.y < 50;
 	}
 };
 
 struct Game : MyApp::Scene{
 
-	Vec2 PlayerPos;
-	Vec2 PlayerMove;
+	Player player;
 	int Time;
 	Array<Block> blocks;
 
 	void init() override{
-		PlayerPos = Vec2(30,560);
-		PlayerMove = Vec2(0,0);
+		player = Player();
 		Time = 0;
-		blocks.push_back(Block(100, 200));
 	}
 
 	void update() override{
 		Time += 4;
-		//y = Clamp((int)(y-v),0,640);
-		PlayerPos += PlayerMove;
-		PlayerMove.moveBy(0,-0.5);
-		if (Input::MouseL.clicked){
-			PlayerMove.set(PlayerMove.x,10);
-		}
+		float a = Random(300);
+		if (Time % 480 == 0) blocks.push_back(Block(a, a+100));
+		player.update();
 		if (Time % 30 == 0) m_data->score += 10;
-		if (PlayerPos.y < 50) changeScene(L"Result");
-		for (auto block : blocks){
-			block.update();
-			
+		if (player.IsDeath()) changeScene(L"Result");
+		for (int i = 0; i < blocks.size();i++){
+			blocks[i].update();
 		}
 	}
 
@@ -54,13 +73,11 @@ struct Game : MyApp::Scene{
 		TextureAsset(L"Sky").draw();
 		TextureAsset(L"Cloud").draw(480 - fmod(Time/4., 1928), 0);
 		TextureAsset(L"Cloud").draw(480 - fmod(Time/4.+964, 1928), 0);
-		TextureAsset(L"Hakka1").draw(PlayerPos.x,640-PlayerPos.y);
+		player.draw();
+		for (auto block : blocks) block.draw();
 		TextureAsset(L"Ground").draw(480 - fmod(Time, 1928), 0);
 		TextureAsset(L"Ground").draw(480 - fmod(Time+964, 1928), 0);
 		(FontAsset(L"Font"))(L"SCORE:" + Format(m_data->score)).draw(100, 30);
-		for (auto block : blocks){
-			block.draw();
-		}
 	}
 
 };
