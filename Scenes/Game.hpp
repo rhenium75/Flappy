73632a891d9080b9524c3bde25f0e0cac3d4d8cb
Rhenium <Rhenium>
2016-01-21 18:13:19 +0900

    当たり判定と死亡判定を追加

--- Scenes/Game.hpp
+++ Scenes/Game.hpp
@@ -4,6 +4,31 @@
 # include <cmath>
 #include"../Flappy.hpp"
 
+class ScreenShake : public IEffect
+{
+private:
+
+	double m_maxOffset, m_lengthMillisec;
+
+public:
+
+	ScreenShake(double maxOffset, double lengthMillisec)
+		: m_maxOffset(maxOffset)
+		, m_lengthMillisec(lengthMillisec) {}
+
+	bool update(double timeSec) override
+	{
+		const double timeMillisec = timeSec * 1000.0;
+
+		const Vec2 offset = RandomVec2(Circle(m_maxOffset * Max(1.0 - timeMillisec / m_lengthMillisec, 0.0)));
+
+		Graphics2D::SetTransform(Mat3x2::Translate(offset));
+
+		return timeMillisec < m_lengthMillisec;
+	}
+};
+
+
 class Block{
 	public:
 	Rect UpRect, DownRect;
@@ -26,23 +51,29 @@ class Player{
 public:
 	Circle body;
 	Vec2 Move;
+	bool Death;
 	Player(){
-		body.setPos(30,560);
-		body.setSize(100);
+		body.setPos(60,80);
+		body.setSize(40);
 		Move = Vec2(0,0);
+		Death = false;
 	}
 	virtual void update(){
 		body.moveBy(Move);
-		Move.moveBy(0,-0.5);
-		if (Input::MouseL.clicked){
-			Move.set(Move.x, 10);
+		Move.moveBy(0,0.5);
+		//Move.y = Min((int)Move.y,10);
+		if (!IsDeath() && Input::MouseL.clicked){
+			Move.set(Move.x, -10);
 		}
 	}
 	virtual void draw() const {
-		TextureAsset(L"Hakka1").draw(body.x, 640 - body.y);
+		TextureAsset(L"Hakka1").rotate(HalfPi/20*Move.y).draw(body.x-55,body.y-40);
 	}
 	bool IsDeath(){
-		return body.y < 50;
+		return Death;
+	}
+	bool OnGround(){
+		return body.y > 590;
 	}
 };
 
@@ -51,6 +82,7 @@ struct Game : MyApp::Scene{
 	Player player;
 	int Time;
 	Array<Block> blocks;
+	Effect effect;
 
 	void init() override{
 		player = Player();
@@ -58,18 +90,31 @@ struct Game : MyApp::Scene{
 	}
 
 	void update() override{
-		Time += 4;
-		float a = Random(300);
-		if (Time % 480 == 0) blocks.push_back(Block(a, a+100));
+		
 		player.update();
-		if (Time % 30 == 0) m_data->score += 10;
-		if (player.IsDeath()) changeScene(L"Result");
-		for (int i = 0; i < blocks.size();i++){
-			blocks[i].update();
+		if (Time % 480 == 0) {
+			float a = Random(300);
+			blocks.push_back(Block(a, a + 200));
 		}
+		if (!player.IsDeath()) {
+			Time += 4;
+			for (int i = 0; i < blocks.size(); i++) {
+				blocks[i].update();
+			}
+			for (int i = 0; i < blocks.size(); i++) {
+				if (player.body.intersects(blocks[i].UpRect) || player.body.intersects(blocks[i].DownRect)) {
+					player.Death = true;
+					effect.add<ScreenShake>(16, 600);
+				}
+			}
+		}
+
+		if (Time % 30 == 0) m_data->score += 10;
+		if (player.OnGround()) changeScene(L"Result");
 	}
 
 	void draw()const override{
+		effect.update();
 		TextureAsset(L"Sky").draw();
 		TextureAsset(L"Cloud").draw(480 - fmod(Time/4., 1928), 0);
 		TextureAsset(L"Cloud").draw(480 - fmod(Time/4.+964, 1928), 0);
